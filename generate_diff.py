f09c4a0816ada08e7e0bd354cb2636f15bee8ebc
Ruslan Sanigullin <hirudorar07@gmail.com>
2024-11-07 16:25:35 +0300

    add venv & all code

---
+++ generate_diff.py
@@ -0,0 +1,23 @@
+import json
+
+def generate_diff(file_path1, file_path2):
+  with open(file_path1, 'r') as file1, open(file_path2, 'r') as file2:
+    data1 = json.load(file1)
+    data2 = json.load(file2)
+    
+  result = []
+  keys = data1.keys() | data2.keys()
+
+  for key in sorted(keys):
+    if key not in data1:
+      result.append(f"+ {key}: {data2[key]}")
+    elif key not in data2:
+      result.append(f"- {key}: {data1[key]}")
+    elif data1[key] != data2[key]:
+      result.append(f"- {key}: {data1[key]}")
+      result.append(f"+ {key}: {data2[key]}")
+    else:
+      result.append(f"  {key}: {data1[key]}")
+
+  formatted_result = "{\n " + "\n ".join(result) + "\n}"
+  return formatted_result
