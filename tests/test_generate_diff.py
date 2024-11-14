8111cf741859f9ab92f0263f0b9d5351a7b278f6
Ruslan Sanigullin <hirudorar07@gmail.com>
2024-11-15 01:16:17 +0300

    nothing

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -1,5 +1,5 @@
 import pytest
-from gendiff.generate_diff import generate_diff
+from gendiff import generate_diff
 
 
 PLAIN = {
@@ -39,5 +39,4 @@ RESULT = {
     ]
 )
 def test_generate_diff_1(path1, path2, format, diff):
-    print(generate_diff)
     assert generate_diff(path1, path2, format) == open(diff).read()
